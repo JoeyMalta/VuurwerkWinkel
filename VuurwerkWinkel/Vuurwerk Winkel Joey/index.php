e60cfff083dfc638f6c12c01511fc50a03fa774f
justjoeygames@gmail.com <Joeymaltap#1>
2018-12-07 09:19:26 +0100

    Update

---
+++ VuurwerkWinkel/Vuurwerk Winkel Joey/index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>VuurwerkWinkel</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,700">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/css/lightbox.min.css">
+    <link rel="stylesheet" href="assets/css/Pretty-Footer.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header"><a class="navbar-brand" href="#"><img src="assets/img/firework01.png" id="logo"><strong>FireworkWizards</strong></a><button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button></div>
+            <div
+                class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="active" role="presentation"><a href="#">Kerst Korting!</a></li>
+                    <li role="presentation"><a href="#">Onze Folder</a></li>
+                    <li role="presentation"><a href="#">Inloggen</a></li>
+                    <li role="presentation"><a href="#">Aanmelden</a></li>
+                </ul>
+        </div>
+        </div>
+    </nav>
+    <div id="promo" style="background-image: url(&quot;assets/img/background.jpg.jpg&quot;);">
+        <div class="jumbotron">
+            <h1>Knallend Vuurwerk! </h1>
+            <p>Het beste vuurwerk van Nederland! </p>
+            <p><a class="btn btn-info btn-lg" role="button" href="#">Nu 20% Korting!</a></p>
+        </div>
+    </div>
+    <div class="container site-section" id="welcome">
+        <h1>Flitsende Knallers en Schitterende Potten</h1>
+        <p>Fireworkwizards zorgt ervoor dat al het vuurwerk getest word voor perfectie. Al het vuurwerk is volgens de wet goedgekeurd en volledig getest.</p>
+    </div>
+    <div class="dark-section">
+        <div class="container site-section" id="why">
+            <h1>Wat hebben wij te bieden?</h1>
+            <div class="row">
+                <div class="col-md-4 item"><i class="glyphicon glyphicon-leaf"></i>
+                    <h2>Efficient voor de economie</h2>
+                    <p>Door de stoffen die wij gebruiken blijven de bomen en planten gezond.&nbsp; </p>
+                </div>
+                <div class="col-md-4 item"><i class="fa fa-money"></i>
+                    <h2>Word goedkoop geproduceerd</h2>
+                    <p>De producten worden met plantaardige stoffen gemaakt dus kost het minder.&nbsp; </p>
+                </div>
+                <div class="col-md-4 item"><i class="fa fa-user-circle-o"></i>
+                    <h2>Fair trade</h2>
+                    <p>De fabrieken die het vuurwerk creeren krijgen eerlijk hun loon.&nbsp; </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container site-section" id="gallery">
+        <h1>Onze Top Producten</h1>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="thumbnail"><a href="assets/img/vuurwerk-crazy-fire.jpg" target="_blank" data-lightbox="cakes"><img class="img-responsive" src="assets/img/vuurwerk-crazy-fire.jpg"></a></div>
+            </div>
+            <div class="col-md-4">
+                <div class="thumbnail"><a href="assets/img/vuurwerk-partypakket-groot.jpg" target="_blank" data-lightbox="cakes"><img class="img-responsive" src="assets/img/vuurwerk-partypakket-groot.jpg"></a></div>
+            </div>
+            <div class="col-md-4">
+                <div class="thumbnail"><a href="assets/img/vuurwerk-crazy-fire.jpg" target="_blank" data-lightbox="cakes"><img class="img-responsive" src="assets/img/vuurwerk-crazy-fire.jpg"></a></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="thumbnail"><a href="assets/img/vuurwerk-crazy-fire.jpg" target="_blank" data-lightbox="cakes"><img class="img-responsive" src="assets/img/vuurwerk-crazy-fire.jpg"></a></div>
+            </div>
+            <div class="col-md-4">
+                <div class="thumbnail"><a href="assets/img/vuurwerk-partypakket-groot.jpg" target="_blank" data-lightbox="cakes"><img class="img-responsive" src="assets/img/vuurwerk-partypakket-groot.jpg"></a></div>
+            </div>
+            <div class="col-md-4">
+                <div class="thumbnail"><a href="assets/img/vuurwerk-crazy-fire.jpg" target="_blank" data-lightbox="cakes"><img class="img-responsive" src="assets/img/vuurwerk-crazy-fire.jpg"></a></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="thumbnail"><a href="assets/img/vuurwerk-crazy-fire.jpg" target="_blank" data-lightbox="cakes"><img class="img-responsive" src="assets/img/vuurwerk-crazy-fire.jpg"></a></div>
+            </div>
+            <div class="col-md-4">
+                <div class="thumbnail"><a href="assets/img/vuurwerk-partypakket-groot.jpg" target="_blank" data-lightbox="cakes"><img class="img-responsive" src="assets/img/vuurwerk-partypakket-groot.jpg"></a></div>
+            </div>
+            <div class="col-md-4">
+                <div class="thumbnail"><a href="assets/img/vuurwerk-crazy-fire.jpg" target="_blank" data-lightbox="cakes"><img class="img-responsive" src="assets/img/vuurwerk-crazy-fire.jpg"></a></div>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-sm-6 footer-navigation">
+                    <h3><a href="#">Firework Wizards</a></h3>
+                    <p class="links"><a href="#">Joey</a><strong> · </strong><a href="#">Bartek</a><strong> · Shaif</strong><a href="#"></a><strong> </strong></p>
+                    <p class="company-name">FireworkWizards © 2018</p>
+                </div>
+                <div class="col-md-4 col-sm-6 footer-contacts">
+                    <div><span class="fa fa-map-marker footer-contacts-icon"> </span>
+                        <p><span class="new-line-span">Vuurwerk Straat 98B</span> Rotterdam, Zuidholland</p>
+                    </div>
+                    <div><i class="fa fa-phone footer-contacts-icon"></i>
+                        <p class="footer-center-info email text-left"> +010 9287611&nbsp;</p>
+                    </div>
+                    <div><i class="fa fa-envelope footer-contacts-icon"></i>
+                        <p> <a href="#" target="_blank">Fireworkwizards@gmail.com</a></p>
+                    </div>
+                </div>
+                <div class="clearfix visible-sm-block"></div>
+                <div class="col-md-4 footer-about">
+                    <h4>Over ons</h4>
+                    <p> Wij zijn een fair trade vuurwerk bedrijf dat zich richt op een beter millieu maar toch mooi vuurwerk.Meer over ons weten? volg ons dan op social media!</p>
+                    <div class="social-links social-icons"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-linkedin"></i></a><a href="#"><i class="fa fa-github"></i></a></div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/js/lightbox-plus-jquery.min.js"></script>
+</body>
+
+</html>
